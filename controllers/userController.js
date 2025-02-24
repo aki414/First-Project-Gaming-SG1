9185de75e7f1f576da8f2076063d54ddb376f406
aki414 <akremsedki21@gmail.com>
2025-02-24 15:54:17 +0100

    t

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,3 +14,63 @@ exports.createUser = async (req, res) => {
     });
   }
 };
+exports.updateUser = async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json({
+      message: "User updated !!!!",
+      data: { updatedUser },
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Fail !!!!",
+      error: error,
+    });
+  }
+};
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const allUsers = await User.find();
+    res.status(200).json({
+      message: "All Users !!!!",
+      data: { allUsers },
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Fail !!!!",
+      error: error,
+    });
+  }
+};
+exports.deleteUser = async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      message: "User deleted !!!!",
+      data: { deletedUser },
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Fail !!!!",
+      error: error,
+    });
+  }
+};
+
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json({
+      message: "User found !!!!",
+      data: { user },
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Fail !!!!",
+      error: error,
+    });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { createUser } = require("../controllers/userController");
+const { createUser, updateUser, getAllUsers, deleteUser, getUserById } = require("../controllers/userController");
 const Router = express.Router();
 Router.route("/").post(createUser);
+Router.route("/:id").patch(updateUser);
+Router.route("/").get(getAllUsers);
+Router.route("/:id").delete(deleteUser);
+Router.route("/:id").get(getUserById);
 module.exports = Router;
